f95fda473638b938363fadcfaf8c091a379aef64
Marc Valdez <127009880+marc-valdez@users.noreply.github.com>
2024-09-17 13:51:55 +0800

    Safe areas

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,9 +1,14 @@
 import { View, Text } from 'react-native';
+import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function SettingsLayout() {
+  const insets = useSafeAreaInsets();
+
   return (
-    <View>
-      <Text>Settings</Text>
-    </View>
+    <SafeAreaProvider>
+      <View style={{ flex: 1, paddingTop: insets.top }}>
+        <Text>Settings</Text>
+      </View>
+    </SafeAreaProvider>
   );
 }

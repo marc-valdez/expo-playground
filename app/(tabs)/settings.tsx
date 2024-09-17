6538bd6fdd66bc72e2b260899097f9244db8ada2
Marc Valdez <127009880+marc-valdez@users.noreply.github.com>
2024-09-17 14:50:16 +0800

    Fonts

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,14 +1,49 @@
-import { View, Text } from 'react-native';
+import { Inter_900Black, useFonts } from '@expo-google-fonts/inter';
+import { Ionicons } from '@expo/vector-icons';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function SettingsLayout() {
   const insets = useSafeAreaInsets();
+  const [loaded, error] = useFonts({
+    Inter_900Black,
+    'Inter-SemiBoldItalic': 'https://rsms.me/inter/font-files/Inter-SemiBoldItalic.otf?v=3.12',
+  })
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+  
+  if (!loaded && !error) {
+    return null;
+  }
 
   return (
     <SafeAreaProvider>
-      <View style={{ flex: 1, paddingTop: insets.top }}>
-        <Text>Settings</Text>
+      <View style={ styles.container }>
+        <View style={ styles.settings }>
+          <Ionicons name="settings-outline" size={20} color="black" />
+          <Text style={{ fontFamily: 'Inter_900Black', paddingLeft: 10 }}>Settings</Text>
+        </View>
+        <Text style={{ fontFamily: 'Inter-SemiBoldItalic', fontSize: 36 }}>Online Font: Inter-SemiBoldItalic</Text>
       </View>
     </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  settings: {
+    flexDirection: 'row',
+  }
+});

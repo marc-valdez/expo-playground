5f881b2e332893cccf82741efe470fcaf1ecd62d
Marc Valdez <127009880+marc-valdez@users.noreply.github.com>
2024-09-17 17:11:02 +0800

    Color themes + Animation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,6 +4,8 @@ export default function TabLayout() {
   return (
     <Tabs>
       <Tabs.Screen name="(home)" />
+      <Tabs.Screen name="auto-dark-mode" />
+      <Tabs.Screen name="animation-test" />
       <Tabs.Screen name="settings" />
     </Tabs>
   );
